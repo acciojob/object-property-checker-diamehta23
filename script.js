2506b76f24c618faef4233143be1e23fb34f3c08
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-14 07:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
 
 function hasKey(key) {
-  
-  return sampleObject.hasOwnProperty(key);
+  return key in sampleObject;
 }
-
-}
-
-// Do not change the code below
-//const key = prompt("Enter Key.");
-//alert(hasKey(key));
